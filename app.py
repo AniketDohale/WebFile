da465420bcd0767a8b091e08606853f0af2db3ca
AniketDohale <aniket.dohale.2801@gmail.com>
2026-05-06 13:00:35 +0530

    Some Minor Changes

--- app.py
+++ app.py
@@ -100,5 +100,5 @@ def delete():
     parent = full_path.parent.relative_to(BASE_DIR)
     return redirect(f"/{parent}" if str(parent) != "." else "/")
 
-if __name__=="__main__":
-    app.run(debug=True, port=5001)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3001, debug=True)
