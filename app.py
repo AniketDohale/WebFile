7bcde38ac471aa10f9f28092733bdb2052b64a06
AniketDohale <aniket.dohale.2801@gmail.com>
2026-05-06 20:33:04 +0530

    Hide and Show State Implemented

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 app = Flask(__name__)
 app.secret_key = "super_secret_key_for_session"
+
 BASE_DIR = Path(r"D:\Media").resolve()
 # BASE_DIR = Path("/home/raspberry_cli").resolve()
 
@@ -34,25 +35,33 @@ def browse(subpath=""):
 
     items = []
 
+    show_Hidden = request.args.get('hidden', 'false') == 'true'
+
     for item in full_Path.iterdir():
+        if not show_Hidden and item.name.startswith('.'):
+            continue
+
         try:
             items.append({
                 "name": item.name,
                 "is_dir": item.is_dir()
             })
+
         except Exception:
             continue
 
     items.sort(key=lambda x: (not x["is_dir"], x["name"].lower()))
-    return render_template("index.html", items=items, current_path=str(Path(subpath)).replace("\\", "/"))
+    return render_template("index.html", items=items, current_path=str(Path(subpath)).replace("\\", "/"), show_Hidden=show_Hidden)
+
 
 @app.route("/upload", methods=["POST"])
 def upload():
     subpath = request.form.get("path", "")
+    show_Hidden_State = request.form.get("show_hidden", "false")
     full_Path = safe_Path(subpath)
 
     if 'file' not in request.files:
-        flash("No file part")
+        flash("No File Part")
         return redirect(request.referrer)
     
     f = request.files["file"]
@@ -63,7 +72,9 @@ def upload():
     filename = Path(f.filename).name
     f.save(full_Path / filename)
     flash(f"Uploaded: {filename}")
-    return redirect(f"/{subpath}" if subpath else "/")
+
+    target_url = f"/{subpath}" if subpath else "/"
+    return redirect(f"{target_url}?hidden={show_Hidden_State}")
 
 
 @app.route("/download/<path:filepath>")
@@ -80,6 +91,9 @@ def download(filepath):
 def rename():
     old_Path = request.form.get("old_path")
     new_Name = request.form.get("new_name")
+
+    show_Hidden_State = request.form.get("show_hidden", "false")
+
     full_old_Path = safe_Path(old_Path)
 
     if not full_old_Path.exists():
@@ -103,12 +117,16 @@ def rename():
         flash(f"Error: {e}")
 
     parent = full_old_Path.parent.relative_to(BASE_DIR)
-    return redirect(f"/{parent}" if str(parent) != "." else "/")
+    target_path = f"/{parent}" if str(parent) != "." else "/"
+    return redirect(f"{target_path}?hidden={show_Hidden_State}")
 
 
 @app.route("/delete", methods=["POST"])
 def delete():
     target_path = request.form.get("path")
+
+    show_Hidden_State = request.form.get("show_hidden", "false")
+
     full_Path = safe_Path(target_path)
     
     if full_Path == BASE_DIR:
@@ -125,7 +143,8 @@ def delete():
         flash(f"Error: {e}")
 
     parent = full_Path.parent.relative_to(BASE_DIR)
-    return redirect(f"/{parent}" if str(parent) != "." else "/")
+    target_path = f"/{parent}" if str(parent) != "." else "/"
+    return redirect(f"{target_path}?hidden={show_Hidden_State}")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3001, debug=True)
