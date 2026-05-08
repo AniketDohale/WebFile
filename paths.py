966c184e044c9c5eba76a49f31ba447bf0bda5dc
AniketDohale <aniket.dohale.2801@gmail.com>
2026-05-08 11:35:32 +0530

    Improve Linux Home Path Logic

--- paths.py
+++ paths.py
@@ -8,20 +8,24 @@ SYSTEM = platform.system()
 if SYSTEM == "Windows":
     BASE_DIR = Path(r"D:\Media").resolve()
 
-else:
-    BASE_DIR = Path("/home/raspberry_cli").resolve()
+elif SYSTEM == "Linux":
+    BASE_DIR = Path.home().resolve()
 
-TEMP_DIR = BASE_DIR / ".tmp"
+    TEMP_DIR = BASE_DIR / ".tmp"
+    TEMP_DIR.mkdir(parents=True, exist_ok=True)
 
-BASE_DIR.mkdir(parents=True, exist_ok=True)
-TEMP_DIR.mkdir(parents=True, exist_ok=True)
+    os.environ["TMPDIR"] = str(TEMP_DIR)
+    os.environ["TEMP"] = str(TEMP_DIR)
+    os.environ["TMP"] = str(TEMP_DIR)
 
-os.environ["TMPDIR"] = str(TEMP_DIR)
-os.environ["TEMP"] = str(TEMP_DIR)
-os.environ["TMP"] = str(TEMP_DIR)
+    tempfile.tempdir = str(TEMP_DIR)
 
+print("*"*80)
 print("SYSTEM:", SYSTEM)
 print("BASE_DIR:", BASE_DIR)
-print("TEMP_DIR:", TEMP_DIR)
 
-tempfile.tempdir = str(TEMP_DIR)
+if SYSTEM != "Windows":
+    print("TEMP_DIR:", TEMP_DIR)
+else:
+    print("Using System Default Temp", tempfile.gettempdir())
+print("*"*80)
