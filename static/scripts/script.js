3a6f7290edfe7c9b2e36019dfb57bbb56a423595
AniketDohale <aniket.dohale.2801@gmail.com>
2026-05-11 16:08:47 +0530

    Minor Row Bug Fixed

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -334,8 +334,22 @@ document.addEventListener("click", function (e) {
     if (e.target.closest(".menu")) {
         return;
     }
+    if (e.target.closest("a")) {
+        return;
+    }
     const row = e.target.closest(".file-row");
-    if (row && row.dataset.href) {
+    if (!row || !row.dataset.href) {
+        return;
+    }
+    // Check if Row is Folder
+    const isFolder =
+        row.dataset.href.startsWith("/") &&
+        !row.dataset.href.startsWith("/view/");
+    if (isFolder) {
+        // Open Folder in same tab
+        window.location.href = row.dataset.href;
+    } else {
+        // Open file in new tab
         window.open(row.dataset.href, "_blank");
     }
 });
